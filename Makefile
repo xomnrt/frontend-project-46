de2b879be96dd74f9c689f96da709ed2098459d5
xomnrt <xomnrt1@ya.ru>
2023-12-01 22:22:48 +0300

    set eslint

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ install:
 	npm ci
 gendiff:
 	node bin/gendiff.js
+publish:
+	npm publish --dry-run
+lint:
+	npx eslint .
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,4 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number');
 
-
 program.parse();
