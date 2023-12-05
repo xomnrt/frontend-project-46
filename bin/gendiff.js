98450b0584cbd7fc95a72afff63b7d9ba1d5e215
xomnrt <xomnrt1@ya.ru>
2023-12-05 22:48:04 +0300

    proceed with step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format', 'output format');
 
 program.parse();
