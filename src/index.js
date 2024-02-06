c5ffcf761b62840d76c05b409eb2efc742994f7f
xomnrt <xomnrt1@ya.ru>
2024-02-06 16:59:46 +0300

    hexlet tests my private functions :peka:

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ export default function compareFiles(filePath1, filePath2, format) {
   const obj1 = parseObjectFromFile(filePath1);
   const obj2 = parseObjectFromFile(filePath2);
   const diff = genDiff(obj1, obj2);
-  return formatDiff(diff, obj1, obj2, format);
+  return formatDiff(diff, obj1, obj2, format || 'stylish');
 }
